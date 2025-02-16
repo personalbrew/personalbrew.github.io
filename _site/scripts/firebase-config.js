116371a6c999b7527246d0dffe6e8557b6bd7a83
Your Name <pikk49>
2025-02-17 00:58:38 +0100

    add _site files to repo

---
+++ _site/scripts/firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+var firebaseConfig = {
+  apiKey: "AIzaSyBhUktdVbhqUYuw5A27fGlBnqeabiPBtyc",
+  authDomain: "personal-brew-webshop.firebaseapp.com",
+  projectId: "personal-brew-webshop",
+  storageBucket: "personal-brew-webshop.firebasestorage.app",
+  messagingSenderId: "861682226406",
+  appId: "1:861682226406:web:c82193e85ffd33c2c75727"
+};
+
+// Initialize Firebase (optional here if you want to initialize right away)
+firebase.initializeApp(firebaseConfig);
