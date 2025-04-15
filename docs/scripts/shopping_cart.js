b1bb8a73610ea39d079f5d3cdba930a7082f9cb1
Your Name <pikk49>
2025-04-15 23:58:06 +0200

    add logging to logOrder

--- docs/scripts/shopping_cart.js
+++ docs/scripts/shopping_cart.js
@@ -244,17 +244,22 @@ async function logOrder(cartItems, totalPrice, shippingMethod, paypalOrdId, cust
       name: item.name,
       quantity: item.quantity
     }));
-    await db.collection("orders").add({
+    const orderPayload = {
       created: new Date(),
       items: orderData,
       totalPrice: totalPrice,
       shipping: shippingMethod,
       paypalOrderId: paypalOrdId,
       customOrderId: customOrdId
-    });
+    };
+    await db.collection("orders").add(orderPayload);
 
     console.log("Order logged successfully");
   } catch (err) {
+    console.log("Order payload:", orderPayload);
+    console.log("Firebase: ", firebase);
+    console.log("Firebase Firestore: ", firebase.firestore());
+    console.log("Firebase db instance: ", db);
     console.error("Error logging order:", err);
   }
 }
